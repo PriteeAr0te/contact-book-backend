e3afffb1176052f6012c51a2e2cd4298b6cbd255
PriteeAr0te <arotepritee111@gmail.com>
2025-06-02 21:27:00 +0530

    front url updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 app.use(cors({
-    origin: process.env.FRONTEND_URL || "https://smart-contact-book.netlify.app",
+    origin: "https://smart-contact-book.netlify.app",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
